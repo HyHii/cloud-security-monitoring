26539f7a605e4ec884170a666bb6a1a6ac7e16bd
HyHii <nggiahy2k5.1306@gmail.com>
2025-06-12 11:43:44 +0700

    Initial commit

---
+++ backend/lambda_functions/process_cloudtrail.py
@@ -0,0 +1,30 @@
+import boto3
+import json
+import os
+
+SNS_TOPIC_ARN = os.environ.get('arn:aws:sns:ap-southeast-1:369683261967:cloud-security-alerts:edf3baae-eaaf-44a5-be24-1274aeda1b8b')
+
+def lambda_handler(event, context):
+    sns_client = boto3.client('sns')
+    alert_events = []
+
+    for record in event.get('Records', []):
+        message = record.get('Sns', {}).get('Message', '')
+        if 'ConsoleLogin' in message and 'Failed' in message:
+            alert_events.append(message)
+
+    if alert_events:
+        alert_text = f"Cảnh báo đăng nhập thất bại AWS:\n\n" + "\n\n".join(alert_events)
+        sns_client.publish(
+            TopicArn=SNS_TOPIC_ARN,
+            Subject='AWS CloudTrail Security Alert',
+            Message=alert_text
+        )
+        print("Sent alert.")
+    else:
+        print("No suspicious events found.")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Processing done')
+    }
